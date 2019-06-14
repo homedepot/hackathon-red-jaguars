c1ae91529c7e9765014ff34541da439d6168a250
gxv8dqj <gautam_vejendla@homedepot.com>
2019-06-14 12:44:23 -0500

    added bootstrap and not found

--- ui/src/app/App.js
+++ ui/src/app/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import './App.css'
 import Login from '../login/Login'
 import Landing from '../landing/Landing'
 import WatchAuth from '../auth/WatchAuth'
 import Dashboard from '../dashboard/Dashboard';
+import NotFound from '../common/NotFound';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           <Route exact path="/landing" component={Landing} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/" component={Login} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="/not-found"> </Redirect>
         </Switch>
       </WatchAuth>
     </Router>
