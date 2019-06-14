05f4415568accceb69446c71dd2f8e615a163e22
Julia Kim <JULIA_KIM@homedepot.com>
2019-06-14 18:55:39 -0500

    Get rid of redundant word img according to Travis CI

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -45,53 +45,53 @@ export default class Landing extends Component {
                 </a>
               </div>
               <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <img class="card-top" src="..." alt="Card image cap"></img>
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="/" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
             <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <img class="card-top" src="..." alt="Card image cap"></img>
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="/" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
             <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <img class="card-top" src="..." alt="Card image cap"></img>
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="/" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
             </div>
             <div class = "col-3">
             <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <img class="card-top" src="..." alt="Card image cap"></img>
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="/" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
             <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <img class="card-top" src="..." alt="Card image cap"></img>
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="/" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
             <div class="card">
-              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <img class="card-top" src="..." alt="Card image cap"></img>
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="/" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
             </div>
