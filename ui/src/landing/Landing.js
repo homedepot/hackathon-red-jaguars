2dbfe44b6d7d08995317b3fbaf7ded44168302d4
Julia Kim <JULIA_KIM@homedepot.com>
2019-06-14 13:02:10 -0500

    Attempting to use CSS

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
-import logo from '../images/MakeAWishLogo.png'
+import "./Landing.css"
+import MakeAWishLogo from '../images/MakeAWishLogo.png'
+import GalaxyLogo from '../images/Galaxy.png'
+
 
 export default class Landing extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the Hackathon Landing Page</h1>
-        <img src={logo} alt="Logo" width="100px" height="25px"/>
-        <button title="Log In/Register" class="button primary"><a href = '/login'>Log In/Register Button</a></button>
+        <body>
+          <div id="banner">
+            <div class="inline-block">
+              <img src={MakeAWishLogo} alt="MakeAWishLogo" width= "300px" height= "100px"/>
+            </div>
+
+            <div class="inline-block">
+              <img src={GalaxyLogo} alt="Galaxy" width="500px" height="250px"/>
+            </div>
+          </div>
+        </body>
       </div>
     )
   }
 }
+
+
