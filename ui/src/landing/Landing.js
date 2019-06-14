55dcacad5eb3108af40819b44beb39e80aecd6a2
Julia Kim <JULIA_KIM@homedepot.com>
2019-06-14 15:23:51 -0500

    Playing with bootstrap

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -5,13 +5,23 @@ import Logo_Galaxy from './Galaxy.png'
 export default class Landing extends Component {
   render() {
     return (
-      <div>
+      <div> 
         <nav class="navbar navbar-light bg-light">
           <a class="navbar-brand" href="/">
-            <img src={Logo_MakeWish} width= "20%" height= "auto" class = "img-responsive" alt=""></img>
-            <img src={Logo_Galaxy} width= "20%" height= "auto" class = "img-responsive" alt=""></img>
+            <div class="container">
+              <div class="row">
+                <div class="col">
+                  <img src={Logo_MakeWish} width= "100%" height= "auto" class = "img-responsive" alt=""></img>
+                </div>
+                <div class="col-">
+                  <img src={Logo_Galaxy} width= "20%" height= "auto" class = "img-responsive" alt=""></img>
+                </div>
+                <div class="text-left">
+                  <button type="button" class="btn btn-primary"><a href = "/login"><p class="text-white">Login/Register</p></a></button>       
+                </div>
+              </div>
+            </div>
           </a>
-          <button type="button" class="btn btn-primary"><a href = "/login"><p class="text-white">Login/Register</p></a></button>       
         </nav>
       </div>
     )
