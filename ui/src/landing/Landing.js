19323bfa86089207b0c114f9b11431d6ba9a52a0
Brandt Swanson <brandtswan@gmail.com>
2019-06-14 17:09:13 -0500

    Updated Login and added cards to landing

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -20,31 +20,83 @@ export default class Landing extends Component {
         </div>
         <div class = "container">
           <div class = "row">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-              <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-              </ol>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img class="d-block w-100" src={Logo_Galaxy_Colour} alt="First slide"></img>
+            <div class = "col-9">
+              <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src={Logo_Galaxy_Colour} alt="First slide"></img>
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src={Logo_MakeWish} alt="Second slide"></img>
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src={Logo_MakeWish_Dark_Blue} alt="Third slide"></img>
+                  </div>
                 </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src={Logo_MakeWish} alt="Second slide"></img>
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src={Logo_MakeWish_Dark_Blue} alt="Third slide"></img>
-                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
               </div>
-              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
+              <div class="card">
+              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+            </div>
+            <div class = "col-3">
+            <div class="card">
+              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="..." alt="Card image cap"></img>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
             </div>
           </div>
         </div>
--- ui/src/login/Login.js
+++ ui/src/login/Login.js
@@ -60,79 +60,48 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the Make-A-Wish Hackathon starter!!</h1>
-        <div className={'login-container'}>
-          <div>
-            <h2>Register</h2>
-            <form
-              className="registration-form"
-              onSubmit={this.createUser}
-              data-register-form
-            >
-              First Name:
-              <input
-                type="text"
-                data-register-first-name
-                onChange={event =>
-                  this.handleFormFieldChange('firstName', event)
-                }
-              />
-              Last Name:
-              <input
-                type="text"
-                data-register-last-name
-                onChange={event =>
-                  this.handleFormFieldChange('lastName', event)
-                }
-              />
-              Username:
-              <input
-                type="text"
-                data-register-username
-                onChange={event =>
-                  this.handleFormFieldChange('username', event)
-                }
-              />
-              Password:
-              <input
-                type="password"
-                data-register-password
-                onChange={event =>
-                  this.handleFormFieldChange('password', event)
-                }
-              />
-              <input type="submit" value="Submit!" />
+        <div class = "container">
+          <div class = "row">
+            <div class = "col-6">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">First Name</label>
+                <input type="string" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Last Name</label>
+                <input type="string" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Username</label>
+                <input type="string" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-          </div>
-
-          <div>
-            <h2>Login</h2>
-            <form
-              className="login-form"
-              onSubmit={this.loginUser}
-              data-login-form
-            >
-              Username:{' '}
-              <input
-                type="text"
-                data-login-username
-                onChange={event =>
-                  this.handleFormFieldChange('username', event)
-                }
-              />
-              Password:{' '}
-              <input
-                type="password"
-                data-login-password
-                onChange={event =>
-                  this.handleFormFieldChange('password', event)
-                }
-              />
-              <input type="submit" value="Submit!" />
+            </div>
+            <div class = "col-6">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Username</label>
+                <input type="string" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+            </div>
           </div>
         </div>
-        <a href= './dashboard'><p>GO HERE</p></a>
       </div>
     )
   }
