cbb5ce98dbd00bebd966f5e69d525a4f135d7b66
Julia Kim <JULIA_KIM@homedepot.com>
2019-06-14 11:31:34 -0500

    Add logo and button to login/register page

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
+import logo from '../images/MakeAWishLogo.png'
 
 export default class Landing extends Component {
   render() {
-    return <h1>Welcome to the Hackathon Landing Page</h1>
+    return (
+      <div>
+        <h1>Welcome to the Hackathon Landing Page</h1>
+        <img src={logo} alt="Logo" width="100px" height="25px"/>
+        <button title="Log In/Register" class="button primary"><a href = '/login'>Log In/Register Button</a></button>
+      </div>
+    )
   }
 }
