c46e92ae3ad3e983be010e6d8edf74556e9a9ec9
Brandt Swanson <brandtswan@gmail.com>
2019-06-14 16:35:52 -0500

    Cleaned up code

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -6,48 +6,49 @@ import Logo_Galaxy_Colour from '../images/Logo_Galaxy_Colour.png'
 export default class Landing extends Component {
   render() {
     return (
-        <div>
-          <div class = "container">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-light fixed-top" id="mainNav" padding = "400">
-        <img src="Logo_MakeWish.png" width= "12%" height= "auto" class = "img-responsive" alt=""></img>
-        <img src="Logo_Galaxy_Colour.png" width= "12%" height= "auto" class = "img-responsive" alt=""></img>
-<ul class="navbar-nav ml-auto">
-                   <li class="nav-item">
-                     <a class="btn btn-primary" href="/login" role="button">Login/Register</a>
-                   </li>
-                 </ul>
-    </nav></div>
-<div class = "container">
-<div class = "row">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src={Logo_Galaxy_Colour} alt="First slide"></img>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src={Logo_MakeWish} alt="Second slide"></img>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src={Logo_MakeWish_Dark_Blue} alt="Third slide"></img>
+      <div>
+        <div class = "container">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-light fixed-top" id="mainNav" padding = "400">
+            <img src="Logo_MakeWish.png" width= "12%" height= "auto" class = "img-responsive" alt=""></img>
+            <img src="Logo_Galaxy_Colour.png" width= "12%" height= "auto" class = "img-responsive" alt=""></img>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="btn btn-primary" href="/login" role="button">Login/Register</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class = "container">
+          <div class = "row">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src={Logo_Galaxy_Colour} alt="First slide"></img>
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src={Logo_MakeWish} alt="Second slide"></img>
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src={Logo_MakeWish_Dark_Blue} alt="Third slide"></img>
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
             </div>
           </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
         </div>
-        </div>
-        </div>
-        </div>
+      </div>
     )
   }
 }
