f8b83f8345d31d856f94bbbf5cdfc28fe18d5c31
Sandeep Jamithi Reddy <sandeep_jamithireddy@homedepot.com>
2019-06-14 08:59:15 -0500

    (sandeep): git check

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send('hello sandeep')
+  res.send('hello sandeep jam')
 })
 
 module.exports = router
