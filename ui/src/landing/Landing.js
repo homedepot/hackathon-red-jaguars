9cffacfa657ff3c4cd0cf0fd164c6aca106f5a0c
Brandt Swanson <brandtswan@gmail.com>
2019-06-14 13:00:59 -0500

    Added logo

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import Logo_MakeWish from './Logo_MakeWish.png'
 
 export default class Landing extends Component {
   render() {
     return (
       <div>
         <nav class="navbar navbar-light bg-light">
-          <a class="navbar-brand" href="#">
-            <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""></img>
-            Bootstrap
+          <a class="navbar-brand" href="/">
+            <img src={Logo_MakeWish} width="50" height="50" class="d-inline-block align-top" alt=""></img>
+            Make-A-Wish
           </a>
           <a href = '/login'><p>Login/Register here</p></a>
         </nav>
