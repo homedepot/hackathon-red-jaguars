2f3427168ff82a0d734c34dbbd38e9d05b52bb2d
Julia Kim <JULIA_KIM@homedepot.com>
2019-06-14 13:55:53 -0500

    Button added

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Logo_MakeWish from './Logo_MakeWish.png'
+import Logo_Galaxy from './Galaxy.png'
 
 export default class Landing extends Component {
   render() {
@@ -8,8 +9,9 @@ export default class Landing extends Component {
         <nav class="navbar navbar-light bg-light">
           <a class="navbar-brand" href="/">
             <img src={Logo_MakeWish} width= "20%" height= "auto" class = "img-responsive" alt=""></img>
+            <img src={Logo_Galaxy} width= "20%" height= "auto" class = "img-responsive" alt=""></img>
           </a>
-          <a href = '/login'><p>Login/Register here</p></a>
+          <button type="button" class="btn btn-primary"><a href = "/login"><p class="text-white">Login/Register</p></a></button>       
         </nav>
       </div>
     )
