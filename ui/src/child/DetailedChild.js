550f4b5568a29283c3280d304465b27de5890e3d
Julia Kim <JULIA_KIM@homedepot.com>
2019-06-15 12:31:47 -0500

    Attempting to fix 500 issue

--- ui/src/child/DetailedChild.js
+++ ui/src/child/DetailedChild.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import { withRouter } from 'react-router-dom'
 import Beemo from './beemo.jpg'
 import HomeDepot from './homedepot.jpg'
@@ -17,104 +18,118 @@ class DetailedChild extends Component {
         e.preventDefault();
         this.props.history.push("/dashboard");
     }
+
+    /*getChild= function() {
+        var obj
+        const {match} = this.props
+        const id = match.params.id
+        axios
+        .get(`${this.expressDomain}/wish/findOneById/${id}`)
+        .then(response => {
+            console.log("data", response.data)
+            obj = response.data
+            console.log("obj", obj)
+            console.log("obj name", obj.firstName)
+            console.log("obj age", obj.age)
+        })
+    }*/
+
     render() {
-        //const {match} = this.props
-        //const id = match.params.id
         return (
             <div>
-                <td className="sameLine"><button className="fancyButtons"  onClick={this.goBack}>Go Back To Dashboard</button></td>
-                <div class="row">
-                    <div class="col">
-                    <div class="container">
-                        <div class="card">
-                            <img src={Beemo} class="img-fluid rounded-circle" alt="..."></img>
-                            <div class="card-body">
-                                <h5 class="card-title">User Photo</h5>
+                <button className="fancyButtons"  onClick={this.goBack}>Go Back To Dashboard</button>
+                <div className="row">
+                    <div className="col">
+                    <div className="container">
+                        <div className="card">
+                            <img src={Beemo} className="img-fluid rounded-circle" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">User Photo</h5>
                             </div>
                         </div>
                         </div>
-                        <div class="card">
-                            <img src={Wish} class="img-fluid" alt="..."></img>
-                            <div class="card-body">
-                                <h5 class="card-title">Type of Wish</h5>
+                        <div className="card">
+                            <img src={Wish} className="img-fluid" alt="..."></img>
+                            <div className="card-body">
+                                <h5 className="card-title">Type of Wish</h5>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Name</h5>
+                    <div className="col-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Name</h5>
                             </div>
                         </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Age</h5>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Age</h5>
                             </div>
                         </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Hometown</h5>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Hometown</h5>
                             </div>
                         </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Illness Summary</h5>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Illness Summary</h5>
                             </div>
                         </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Wish Details</h5>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Wish Details</h5>
                             </div> 
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                            <img src={HomeDepot} class="img-fluid" alt="..."></img>
-                                <h5 class="card-title">Sponsor Photo</h5>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-body">
+                            <img src={HomeDepot} className="img-fluid" alt="..."></img>
+                                <h5 className="card-title">Sponsor Photo</h5>
                             </div>
                         </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Sponsor ID</h5>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Sponsor ID</h5>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                        <div class="col-lg">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src={BigPic2} class="card-img-top" alt="..."></img>
-                                    <h5 class="card-title">Pic</h5>
+                <div className="row">
+                        <div className="col-lg">
+                            <div className="card">
+                                <div className="card-body">
+                                    <img src={BigPic2} className="card-img-top" alt="..."></img>
+                                    <h5 className="card-title">Pic</h5>
                                 </div>
                             </div>
                         </div> 
-                        <div class="col-lg">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src={BigPic} class="img-fluid" alt="..."></img>
-                                    <h5 class="card-title">Pic</h5>
+                        <div className="col-lg">
+                            <div className="card">
+                                <div className="card-body">
+                                    <img src={BigPic} className="img-fluid" alt="..."></img>
+                                    <h5 className="card-title">Pic</h5>
                                 </div>
                             </div>
                         </div> 
-                        <div class="col-lg">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div align="center" class="embed-responsive embed-responsive-16by9">
-                                        <video autoplay loop class="embed-responsive-item">
+                        <div className="col-lg">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div align="center" className="embed-responsive embed-responsive-16by9">
+                                        <video autoPlay loop className="embed-responsive-item">
                                             <source src={ShortVid} type="video/mp4"></source>
                                         </video>
                                     </div>
-                                    <h5 class="card-title">Play Video</h5>
+                                    <h5 className="card-title">Play Video</h5>
                                 </div>
                             </div>
                         </div>  
-                        <div class="col-lg">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="..." class="card-img-top" alt="..."></img>
-                                    <h5 class="card-title">Play Sound</h5>
+                        <div className="col-lg">
+                            <div className="card">
+                                <div className="card-body">
+                                    <img src="..." className="card-img-top" alt="..."></img>
+                                    <h5 className="card-title">Play Sound</h5>
                                 </div>
                             </div>
                         </div>  
