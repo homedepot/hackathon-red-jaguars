50cddfb08806955229f21962cfef0daf210b2082
Julia Kim <JULIA_KIM@homedepot.com>
2019-06-16 10:26:16 -0500

    Experiment with roles

--- ui/src/child/DetailedChild.js
+++ ui/src/child/DetailedChild.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom'
-import Beemo from './beemo.jpg'
-import HomeDepot from './homedepot.jpg'
-import BigPic from './bigpic.jpg'
-import BigPic2 from './bigpic2.jpg'
-import ShortVid from './shortvid.mp4'
-import Wish from '../images/Alien_Icon.png'
+//import Beemo from './beemo.jpg'
+//import HomeDepot from './homedepot.jpg'
+//import BigPic from './bigpic.jpg'
+//import BigPic2 from './bigpic2.jpg'
+//import ShortVid from './shortvid.mp4'
+import Alien_Icon from '../images/Alien_Icon.png'
+import Astronaut_Icon from '../images/Astronaut_Icon.png'
+import Rocket_Icon from '../images/Rocket_Icon.png'
+import Telescope_Icon from '../images/Telescope_Icon.png'
 
 const DetailedChild = (props)=>{
     const wish= props.location.state.wish;
@@ -20,17 +23,17 @@ const DetailedChild = (props)=>{
           photo: URL.createObjectURL(e.target.files[0])
         })
     }
-    const onAudioUpload = (e) => {
+    /*const onAudioUpload = (e) => {
         this.setState ({
           audio: URL.createObjectURL(e.target.files[0])
         })
-      }
+    }*/
     const resetFile = e => {
         e.target.value = null;
     };
-    const handleFormFieldChange = (key, { target: { value } }) => {
+    /*const handleFormFieldChange = (key, { target: { value } }) => {
         this.setState({ [key]: value })
-      }
+    }*/
     // const getChild = () => {
     //     const {match} = props
     //     const id = match.location.state.id
@@ -45,23 +48,24 @@ const DetailedChild = (props)=>{
             <div>
                 <button className="fancyButtons"  onClick={goBack}>Go Back To Dashboard</button>
                 <div className="row">
-                    <div className="col">
+                    <div className="col-3">
                     <div className="container">
                         <div className="card">
-                            <img src={Beemo} className="img-fluid rounded-circle" alt="..."></img>
+                            {/*<img src={Beemo} className="img-fluid rounded-circle" alt="..."></img>*/}
                             <div className="card-body">
-                            <input className="inputFile" id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                            <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
                             </div>
                         </div>
                         </div>
                         <div className="card">
-                            <img src={Wish} className="img-fluid" alt="..."></img>
+                            {/*<img src={wish.wishType==="GO Somewhere!" ? {Rocket_Icon} : wish.wishType==="MEET Someone!" ? {Alien_Icon} : wish.wishType==="BE Someone!" ? {Astronaut_Icon} : {Telescope_Icon}} className="img-fluid rounded-circle" alt="..."></img>*/}
+                            <img src={Rocket_Icon} className="img-fluid rounded-circle" alt="..."></img>
                             <div className="card-body">
-                                <h5 className="card-title">Type of Wish</h5>
+                                <h5 className="card-title">Type of Wish</h5> {wish.wishType}
                             </div>
                         </div>
                     </div>
-                    <div className="col-6">
+                    <div className="col-5">
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Name</h5> {wish.firstName}
@@ -91,8 +95,8 @@ const DetailedChild = (props)=>{
                     <div className="col">
                         <div className="card">
                             <div className="card-body">
-                                <img src={HomeDepot} className="img-fluid" alt="..."></img>
-                                <input className="inputFile" id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                {/*<img src={HomeDepot} className="img-fluid" alt="..."></img>*/}
+                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
                             </div>
                         </div>
                         <div className="card">
@@ -103,42 +107,40 @@ const DetailedChild = (props)=>{
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-lg">
+                    <div className="col">
                         <div className="card">
                             <div className="card-body">
-                                <img src={BigPic2} className="card-img-top" alt="..."></img>
-                                <input className="inputFile" id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                {/*<img src={BigPic2} className="card-img-top" alt="..."></img>*/}
+                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
                             </div>
                         </div>
                     </div> 
-                    <div className="col-lg">
+                    <div className="col">
                         <div className="card">
                             <div className="card-body">
-                                <img src={BigPic} className="img-fluid" alt="..."></img>
-                                <input className="inputFile" id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                {/*img src={BigPic} className="img-fluid" alt="..."></img>*/}
+                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
                             </div>
                         </div>
                     </div> 
-                    <div className="col-lg">
+                    <div className="col">
                         <div className="card">
                             <div className="card-body">
-                                <div align="center" className="embed-responsive embed-responsive-16by9">
+                                {/*<div align="center" className="embed-responsive embed-responsive-16by9">
                                     <video className="embed-responsive-item">
                                         <source src={ShortVid} type="video/mp4"></source>
                                     </video>
-                                </div>
-                                <a href="#" class="btn btn-primary">Upload Video</a>
+                                </div>*/}
+                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
                             </div>
                         </div>
                     </div>  
-                    <div className="col-lg">
+                    <div className="col">
                         <div className="card">
                             <div className="card-body">
                                 {/*<img src="..." className="img-fluid" alt="..."></img>*/}
-                                <label class="btn btn-primary" for="my-file-selector">
-                                <input id="my-file-selector" type="file" class="d-none" onChange={onAudioUpload} onClick={resetFile}/>
-                                    Button Text Here
-                                </label>
+                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                
                             </div>
                         </div>
                     </div>  
