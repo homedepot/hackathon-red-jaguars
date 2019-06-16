23ed40883019f7472f13fa35cef840a05c880db6
Julia Kim <JULIA_KIM@homedepot.com>
2019-06-16 11:33:57 -0500

    Changed some formatting

--- ui/src/child/DetailedChild.js
+++ ui/src/child/DetailedChild.js
@@ -12,8 +12,8 @@ import Telescope_Icon from '../images/Telescope_Icon.png'
 
 const DetailedChild = (props)=>{
     const wish= props.location.state.wish;
-    const role = props.location.state.role;
-
+    //const role = props.location.state.role;
+    const role = "manager";
     const goBack = (e) => {
         e.preventDefault();
         this.props.history.push("/dashboard");
@@ -31,6 +31,7 @@ const DetailedChild = (props)=>{
     const resetFile = e => {
         e.target.value = null;
     };
+    
     /*const handleFormFieldChange = (key, { target: { value } }) => {
         this.setState({ [key]: value })
     }*/
@@ -46,104 +47,157 @@ const DetailedChild = (props)=>{
         console.log("role",role)
         return (
             <div>
-                <button className="fancyButtons"  onClick={goBack}>Go Back To Dashboard</button>
-                <div className="row">
-                    <div className="col-3">
-                    <div className="container">
-                        <div className="card">
-                            {/*<img src={Beemo} className="img-fluid rounded-circle" alt="..."></img>*/}
-                            <div className="card-body">
-                            <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3">
+                            <div align="center">
+                            <button type="button" className="btn btn-primary btn-large btn-block"  onClick={goBack}>Go Back To Dashboard</button>
+
                             </div>
                         </div>
-                        </div>
-                        <div className="card">
-                            {/*<img src={wish.wishType==="GO Somewhere!" ? {Rocket_Icon} : wish.wishType==="MEET Someone!" ? {Alien_Icon} : wish.wishType==="BE Someone!" ? {Astronaut_Icon} : {Telescope_Icon}} className="img-fluid rounded-circle" alt="..."></img>*/}
-                            <img src={Rocket_Icon} className="img-fluid rounded-circle" alt="..."></img>
-                            <div className="card-body">
-                                <h5 className="card-title">Type of Wish</h5> {wish.wishType}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-5">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Name</h5> {wish.firstName}
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Age</h5> {wish.age}
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Hometown</h5> {wish.homeTown}
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Illness Summary</h5> {wish.illness}
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Wish Details</h5> {wish.wishDetail}
-                            </div> 
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-body">
-                                {/*<img src={HomeDepot} className="img-fluid" alt="..."></img>*/}
-                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Sponsor ID</h5> {wish.orgId}
+                        <div className="col-5">
+                            <div className="card" align="center">
+                                <div className="card-body">
+                                <h5 className="card-tite">Date Wished</h5> {wish.wishDate}
+
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-body">
-                                {/*<img src={BigPic2} className="card-img-top" alt="..."></img>*/}
-                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                    <div className="row">
+                        <h1></h1>
+                    </div>
+                    <div className="row">
+                        <div className="col-3">
+                            <div className="card">
+                                {/*<img src={Beemo} className="img-fluid rounded-circle" alt="..."></img>*/}
+                                <div className="card-body">
+                                    <input className={role==="manager" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                    {/*<img src={Beemo} className="img-fluid rounded-circle" alt="..."></img>*/}
+
+                                    <h5 className="card-title">Child's Picture</h5>
+                                </div>
+                            </div>
+                            <div className="card">
+                                {/*<img src={wish.wishType==="GO Somewhere!" ? {Rocket_Icon} : wish.wishType==="MEET Someone!" ? {Alien_Icon} : wish.wishType==="BE Someone!" ? {Astronaut_Icon} : {Telescope_Icon}} className="img-fluid rounded-circle" alt="..."></img>*/}
+                                <div className="card-body">
+                                    {/*<img src={Beemo} className="img-fluid rounded-circle" alt="..."></img>*/}
+
+                                    <h5 className="card-title">Type of Wish</h5> {wish.wishType}
+                                </div>
                             </div>
                         </div>
-                    </div> 
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-body">
-                                {/*img src={BigPic} className="img-fluid" alt="..."></img>*/}
-                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                        <div className="col-5">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Name</h5> {wish.firstName}
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Age</h5> {wish.age}
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Gender</h5> {wish.gender}
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Hometown</h5> {wish.homeTown}
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Illness Summary</h5> {wish.illness}
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Wish Details</h5> {wish.wishDetail}
+                                </div> 
                             </div>
                         </div>
-                    </div> 
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-body">
-                                {/*<div align="center" className="embed-responsive embed-responsive-16by9">
-                                    <video className="embed-responsive-item">
-                                        <source src={ShortVid} type="video/mp4"></source>
-                                    </video>
-                                </div>*/}
-                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                        <div className="col-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5>Sponsor Logo</h5>
+                                    {/*<img src={HomeDepot} className="img-fluid" alt="..."></img>*/}
+                                    <input className={role==="manager" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Sponsor ID</h5> {wish.orgId}
+                                </div>
                             </div>
                         </div>
-                    </div>  
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-body">
-                                {/*<img src="..." className="img-fluid" alt="..."></img>*/}
-                                <input className={role==="admin" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
-                                
+                    </div>
+                    <div className="row">
+                        <h1></h1>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <div align="center">
+                            <h1>Additional Info</h1>
+
                             </div>
                         </div>
-                    </div>  
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5>Picture</h5>    
+                                    {/*<img src={BigPic2} className="card-img-top" alt="..."></img>*/}
+                                    <input className={role==="manager" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                </div>
+                            </div>
+                        </div> 
+                        <div className="col">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5>Picture</h5>
+                                    {/*img src={BigPic} className="img-fluid" alt="..."></img>*/}
+                                    <input className={role==="manager" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                </div>
+                            </div>
+                        </div> 
+                        <div className="col">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5>Video</h5>
+                                    {/*<div align="center" className="embed-responsive embed-responsive-16by9">
+                                        <video className="embed-responsive-item">
+                                            <source src={ShortVid} type="video/mp4"></source>
+                                        </video>
+                                    </div>*/}
+                                    <input className={role==="manager" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                </div>
+                            </div>
+                        </div>  
+                        <div className="col">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5>Audio</h5>
+                                    {/*<img src="..." className="img-fluid" alt="..."></img>*/}
+                                    <input className={role==="manager" ? "inputFile" : "hidden"} id="file" type="file" onChange={onhandleChange} onClick={resetFile}/>
+                                    
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <div className="row">
+                        <h1></h1>
+                    </div>
+                    <div className="row justify-content-end">
+                        <div className="col">
+                            <div className="text-right">
+                                <button type="button" className="btn btn-primary btn-large">Save Changes</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
